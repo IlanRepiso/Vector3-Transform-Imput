ec49f61480a941331e1d10c078719ade33467479
IlanRepiso <101400193+IlanRepiso@users.noreply.github.com>
2022-05-17 14:28:04 -0300

    Movement

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public Vector3 cubeMovement;
+    public Vector3 cubeRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += cubeMovement;
+        transform.eulerAngles += cubeRotation;
+    }
+}
